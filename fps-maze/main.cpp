fa511db83579c84cee403bbc5fe93a9da004b291
PrateekTh <PrateekTh@users.noreply.github.com>
2021-07-26 23:03:42 +0530

    the pain begins T-T
    started setting up ray tracing T_T

--- fps-maze/main.cpp
+++ fps-maze/main.cpp
@@ -13,6 +13,7 @@ float playerA;
 float miniheight = 16;
 float miniwidth = 16;
 
+float FOV = 3.1459/4;
 
 int main(){
 
@@ -46,7 +47,18 @@ int main(){
         screen[width*height - 1] = '\0';
         WriteConsoleOutputCharacter(console, screen, width * height, {0,0}, &dwbytes );
 
-        
+        for (int n = 0; n<height; n++){
+
+            float RayAngle = (playerA - FOV/2.0f) + ((float)n/(float)width) * FOV;
+
+            float DistanceToWall = 0;
+            bool hitwall = false;
+
+            while(!hitwall){
+
+            }
+
+        }
 
 
     }
