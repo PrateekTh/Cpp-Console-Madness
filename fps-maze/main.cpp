1cf34f027b7f656f67639e58588d641175c5cb2b
PrateekTh <PrateekTh@users.noreply.github.com>
2021-07-26 22:07:46 +0530

    Added the Console, using console functions in Windows.h

--- fps-maze/main.cpp
+++ fps-maze/main.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
+using namespace std;
+
+#include <windows.h>
+
+int height = 40;
+int width = 120;
 
 int main(){
+
+    //create screen
+    wchar_t *screen = new wchar_t[width*height];
+    HANDLE console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(console);
+
+    DWORD dwbytes = 0;
+
+    screen[width*height - 1] = '\0';
+    WriteConsoleOutputCharacter(console, screen, width * height, {0,0}, &dwbytes )
+
 
     return 0;
 }
